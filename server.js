08c92831b6a676f547ebd246057f2b77de842df8
Your Name <you@example.com>
2026-03-13 16:32:10 +0200

    Fix server wildcard route

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "build")));
 
 app.post("/api/chat", async (req, res) => {
@@ -34,7 +33,7 @@ app.post("/api/chat", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
